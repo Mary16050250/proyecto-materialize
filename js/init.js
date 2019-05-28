0493c4d769d6eb0f0f1c5942110512405e9a1fcf
Mary16050250 <mariadelrosario.ulloa@itsn.edu.mx>
2019-05-27 22:36:55 -0500

    Segunda Version

--- js/init.js
+++ js/init.js
@@ -7,33 +7,33 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
+//('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('select');
+   // var instances = M.FormSelect.init(elems, options);
+  //});
 
   // Or with jQuery
 
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+ // $(document).ready(function(){
+   // $('select').formSelect();
+  //});
 
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-  });
+  //document.addEventListener('DOMContentLoaded', function() {
+   /// var elems = document.querySelectorAll('.datepicker');
+   // var instances = M.Datepicker.init(elems, options);
+  //});
 
   // Or with jQuery
 
-  $(document).ready(function(){
-    $('.datepicker').datepicker();
-  });
+  //$(document).ready(function(){
+   // $('.datepicker').datepicker();
+ // });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
+  //document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('.modal');
+    //var instances = M.Modal.init(elems, options);
+  //});
 
   // Or with jQuery
 
@@ -41,19 +41,20 @@
     $('.modal').modal();
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+ // document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('.sidenav');
+    //var instances = M.Sidenav.init(elems, options);
+  //});
 
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  var collapsibleElem = document.querySelector('.collapsible');
-   var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+  //var collapsibleElem = document.querySelector('.collapsible');
+   //var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
   // Or with jQuery
 
- $(document).ready(function() {
-    $('select').material_select();
+ //$(document).ready(function() {
+  //  $('select').material_select();
+
 
     // for HTML5 "required" attribute
     $("select[required]").css({
@@ -62,7 +63,7 @@
       padding: 0,
       width: 0
     });
-  });
+  //});
 
 
 
