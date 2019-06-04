8248f07f45b29627c81beec52a1782c20b71e684
Mary16050250 <mariadelrosario.ulloa@itsn.edu.mx>
2019-06-03 22:46:23 -0500

    Final

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $('select').formSelect();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -64,6 +64,7 @@
       width: 0
     });
   //});
+
 
 
 
